4a0461f6f58c2917824589785a417298e05f8430
= <=>
2022-10-20 00:16:26 -0500

    Migracione de los modelos

--- src/models/categoria-producto.model.ts
+++ src/models/categoria-producto.model.ts
@@ -1,6 +1,23 @@
 import {Entity, model, property} from '@loopback/repository';
 
-@model()
+@model({
+  settings: {
+    foreignKeys: {
+      fk_cat_pro_id_producto: {
+        name: 'fk_cat_pro_id_producto',
+        entity: 'Producto',
+        entityKey: 'id',
+        foreignKey: 'id_producto',
+      },
+      fk_cat_pro_id_categoria: {
+        name: 'fk_cat_pro_id_categoria',
+        entity: 'Categoria',
+        entityKey: 'id',
+        foreignKey: 'id_categoria',
+      },
+    },
+  },
+})
 export class CategoriaProducto extends Entity {
   @property({
     type: 'number',
@@ -28,4 +45,5 @@ export interface CategoriaProductoRelations {
   // describe navigational properties here
 }
 
-export type CategoriaProductoWithRelations = CategoriaProducto & CategoriaProductoRelations;
+export type CategoriaProductoWithRelations = CategoriaProducto &
+  CategoriaProductoRelations;
